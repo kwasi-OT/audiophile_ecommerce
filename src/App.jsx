112e8d8492f0a046aedbaae2e31fe9133f861993
Kwasi O-T <kwasi@kudigo.com>
2024-01-04 23:57:23 +0000

    imported components and setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.scss'
+import Category from './components/Category'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './components/Home'
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import Product from './components/Product'
 
 function App() {
   
   return (
-    <>
-      
-    </>
+    <div>
+      <Router>
+        <Header/>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/category/:name' element={<Category/>}/>
+            <Route exact path='/product/:sku' element={<Product/>}/>
+          </Routes>
+        <Footer/>
+      </Router>
+    </div>
   )
 }
 
