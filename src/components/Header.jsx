d19f4f2a5458475fdb18317da3fab7a395ec5824
Kwasi O-T <kwasi@kudigo.com>
2024-01-05 00:27:14 +0000

    added basic structure to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { TiThMenu } from "react-icons/ti";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Header = () => {
     return (
-        <div>
-        
+        <div className='header'>
+            <div className='headerContainer'>
+                <TiThMenu/>
+                <h1 className='logoText'>audiophile</h1>
+                <ul className='menuList'>
+                    <li>Home</li>
+                    <li>Headphones</li>
+                    <li>Speakers</li>
+                    <li>Earphones</li>
+                </ul>
+                <AiOutlineShoppingCart/>
+            </div>
         </div>
     )
 }
