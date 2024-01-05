d924c52dfb4843c8e6fe41639237681b345022bc
Kwasi O-T <kwasi@kudigo.com>
2024-01-05 01:42:57 +0000

    replaced react-icons with svg icons from assets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import { TiThMenu } from "react-icons/ti";
-import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from 'react-router-dom';
+import logosvg from '../../resources/assets/shared/desktop/logo.svg';
+import cartsvg from '../../resources/assets/shared/desktop/icon-cart.svg';
+import mobilemenu from '../../resources/assets/shared/tablet/icon-hamburger.svg';
 
 const Header = () => {
     return (
         <div className='header'>
             <div className='headerContainer'>
-                <TiThMenu/>
-                <h1 className='logoText'>audiophile</h1>
+                <Link><img src={mobilemenu} alt='mobile menu icon'/></Link>
+                <Link><img src={logosvg} alt='logo'/></Link>
                 <ul className='menuList'>
                     <li><Link className='link'>Home</Link></li>
                     <li><Link className='link'>Headphones</Link></li>
                     <li><Link className='link'>Speakers</Link></li>
                     <li><Link className='link'>Earphones</Link></li>
                 </ul>
-                <AiOutlineShoppingCart/>
+                <Link><img src={cartsvg} alt='cart icon'/></Link>
             </div>
         </div>
     )
